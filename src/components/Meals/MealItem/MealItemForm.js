5b67ba52e68113a2ce031b644969968a00139adb
Vineeth05kumar <Vineeth05kumar>
2024-04-19 22:33:45 +0530

    Upon add on fooditem cart number is updating

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,36 @@
+import { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
+ const[amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount.length < 1 ||
+      enteredAmount.length > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
-          id: 'amount',
+          id: "amount",
           type: "number",
           min: "1",
           max: "5",
@@ -15,7 +38,8 @@ const MealItemForm = (props) => {
           defaultValue: "1",
         }}
       />
-      <button type="click">+ Add</button>
+      <button type="submit">+ Add</button>
+      {!amountIsValid && <p>Please enter a valid Amount</p>}
     </form>
   );
 };
